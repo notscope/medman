fc05390ddcbc26882c3b94f7be15b504302fbe87
notscope <mrlobyy48w@gmail.com>
2025-06-20 02:04:30 +0700

    config adjustment

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ HIGH_THRESHOLD = 0.95 # for automatic deduplication
 LOW_THRESHOLD = 0.85 # for interactive review
 MAX_WORKERS = 8  # Max threads for parallel processing
 MAX_SHA_WORKERS = 16  # Max threads for SHA256 hashing
-MAX_PHASH_WORKERS = 8  # Max threads for perceptual hashing
+MAX_PHASH_WORKERS = 4  # Max threads for perceptual hashing
